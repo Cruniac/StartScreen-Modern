b97f00b86880e94066c04baf079943e24453494d
Michael Burgherr <m.burgherr@gmx.ch>
2014-05-07 21:13:14 +0200

    Replaced CSE with Experimental Custom CSE Search

---
+++ js/google-search.js
@@ -0,0 +1,30 @@
+
+function StartSearch() {
+  console.log('wow'+$('#sbox-query').val());
+
+  $('#searchengine').empty();
+  SearchQuery($('#sbox-query').val());
+}
+
+function SearchQuery(query) {
+
+  var key = 'AIzaSyBlMNsGxO_4Vn8a9odoGbZiW7EXhR6NUq0';
+  var engineid = '005962480996055108906:wpdor7vf40g';
+  var url = 'https://www.googleapis.com/customsearch/v1';
+
+  $.ajax({
+    type: "GET",
+    url: url,
+    data: { key: key, cx: engineid , q: query}
+  }).done(function( response ) {
+
+    //Loop trough each Google search result and make it show up on the screen.
+    for (var i = 0; i < response.items.length; i++) {
+      var item = response.items[i];
+      console.log(item);
+
+      $( "#searchengine" ).append( '<div class="sr-title"><a class="sr-link" href="'+item.link+'">'+item.title+' > '+item.link+'</a></div><div class="sr-box">'+item.snippet+'</div>' );
+    }
+
+  });
+}
