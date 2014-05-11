94069522e497bc83987ba4095804635bf78c9832
Michael Burgherr <m.burgherr@gmx.ch>
2014-05-11 12:41:49 +0200

    Search Engine Overhaul

---
+++ js/search.js
@@ -0,0 +1,64 @@
+/*
+| Info:
+| This is the script that provides the functionality for the search box.
+| This script provides search functionality for a number of Search engines.
+|----------------------------------------------------------------------------
+| Suppoted Search engines:
+| * Google
+| * DuckDuckGo
+| * DuckDuckGo Next
+| * Bing
+| * StartPage
+| * Wolfram Alpha
+*/
+
+//Search Engine Search URL's
+var Google = 'https://www.google.ch/#q=';
+var DDG = 'https://duckduckgo.com/?q=';
+var DDGN = 'https://next.duckduckgo.com/?q=';
+var Bing = 'https://www.bing.com/search?q=';
+var StartPage = 'https://startpage.com/do/search?language=english&cat=web&query=';
+var Wolfram = 'https://www.wolframalpha.com/input/?i=';
+
+var currentEngine = Google;
+
+function Search(engine) {
+	switch(engine) {
+		case 'google':
+			currentEngine = Google;
+			break;
+		case 'ddg':
+			currentEngine = DDG;
+			break;
+		case 'ddgn':
+			currentEngine = DDGN;
+			break;
+		case 'bing':
+			currentEngine = Bing;
+			break;
+		case 'startpage':
+			currentEngine = StartPage;
+			break;
+		case 'wolfram':
+			currentEngine = Wolfram;
+			break;
+	}
+	SubmitSearch();
+}
+
+//Open New window with search engine and query defined.
+function SubmitSearch() {
+	string = window.document.getElementById("sbox-query").value;
+	window.location = currentEngine + string;
+}
+
+//When Enter is pressed, start searching
+function onReturnPress(){
+	console.log('Search Event');
+	$("#sbox-query").keyup(function(event){
+		console.log(event.keyCode);
+		if(event.keyCode == 13){
+			SubmitSearch();
+		}
+	});
+}
